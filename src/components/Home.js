92c9eb022807aab8fcbcfd55c8cf7d4fa5a3b108
Tilak <adireddytilak@gmail.com>
2025-10-30 13:59:46 +0530

    Completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import Navigation from './Navigation'
 const Home = () => {
   return (
     <div>
-        <h1>Welcome to my website</h1>
+        <h1>Welcome to my website!</h1>
     </div>
   )
 }
